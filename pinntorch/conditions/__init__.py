2bf5b02f07f31d6f5407efb42243ab2cdcc09b5f
kasra-kakavand <www.kasrakakavandup@gmail.com>
2026-05-25 16:07:23 +0330

    "Add conditions sub-package init"

---
+++ pinntorch/conditions/__init__.py
@@ -0,0 +1,6 @@
+"""Initial and boundary condition handlers for PINNs."""
+
+from pinntorch.conditions.initial import InitialCondition
+from pinntorch.conditions.boundary import BoundaryCondition
+
+__all__ = ["InitialCondition", "BoundaryCondition"]

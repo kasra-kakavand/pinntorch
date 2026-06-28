43aac5a67dbcbf1e3416fa7bb9fca8515ef181b5
kasra-kakavand <www.kasrakakavandup@gmail.com>
2026-06-28 23:56:43 +0330

    "Add visualization sub-package init"

---
+++ pinntorch/visualization/__init__.py
@@ -0,0 +1,17 @@
+"""Visualization utilities for PINN solutions."""
+
+from pinntorch.visualization.plots import (
+    plot_solution,
+    plot_solution_1d,
+    plot_training_history,
+    plot_comparison,
+    plot_error,
+)
+
+__all__ = [
+    "plot_solution",
+    "plot_solution_1d",
+    "plot_training_history",
+    "plot_comparison",
+    "plot_error",
+]
